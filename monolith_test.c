43d893061c222d5cade923f1fddb706614159667
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-07-07 04:45:33 -0400

    Added test 2

--- monolith_test.c
+++ monolith_test.c
@@ -6,5 +6,10 @@ int main() {
 	vector vec1 = new_vector();
 	rav_push(&vec1, 2);
 	printf("Test 1: %d\n\n", rav_value_at(&vec1, 0));
+
+	vector vec2 = new_vector();
+	int test_arr[] = {4, 3, 2};
+	rav_push_arr(&vec2, test_arr, 3);
+	printf("Test 2: %d\n\n", rav_value_at(&vec2, 0));
 	return 0;
 }
