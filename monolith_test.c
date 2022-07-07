551852428a21ae0790ad1b6906c0a1d07c67cba6
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-07-07 04:27:14 -0400

    Added test1

--- monolith_test.c
+++ monolith_test.c
@@ -2,5 +2,11 @@
 #include "lib/ravectors.h"
 
 int main() {
+	// TEST 1
+	vector vec1 = new_vector();
+	rav_push(&vec1, 2);
+	char* s;
+	rav_to_string(&vec1, s);
+	printf("Test 1: %s", s);
 	return 0;
 }
