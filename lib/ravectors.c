129549e98d76a789c40c16659b3e58f8088b9b36
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-07-07 03:22:34 -0400

    Added implementation templates

---
+++ lib/ravectors.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "ravectors.h"
+
+
+vector new_vector() {
+	// todo
+}
+
+void rav_push(vector *vec, int x) {
+	// todo
+}
+
+void rav_push_arr(vector *vec, int *arr) {
+	// todo
+}
+
+int len(vector* vec) {
+	// todo
+}
+
+int index_of(vector* vec, int x) {
+	// todo
+}
