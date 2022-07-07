ed586175dc02c578198e7d723178f1d5f3667f0f
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-07-07 14:36:12 -0400

    Added declarations for remove and dealloc

--- lib/ravectors.h
+++ lib/ravectors.h
@@ -20,6 +20,8 @@ void rav_push(vector*, int);
 
 void rav_push_arr(vector*, int arr[], int size);
 
+void rav_remove(vector*, unsigned int);
+
 int rav_len(vector*);
 
 int rav_index_of(vector*, int);
@@ -27,5 +29,7 @@ int rav_index_of(vector*, int);
 int rav_value_at(vector*, unsigned int);
 
 char* rav_to_string(vector*, char*);
+
+void rav_dealloc(vector*);
 
 #endif
